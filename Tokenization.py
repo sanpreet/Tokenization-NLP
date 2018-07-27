1ce5eb016d67eec8ca277bd1faf687b9558eb4f2
Sanpreet Singh <sany26.com@gmail.com>
2018-07-27 11:38:11 +0530

    Tokens and ngrams

---
+++ Tokenization.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.util import ngrams
+def possible_combination(text,n):
+    token = nltk.word_tokenize(text)
+    possible_combination_n=ngrams(token,n)
+    return possible_combination_n
+#Writtemn three text
+Text1 = 'My name is Sanpreet Singh'
+Text2 = "You are reading code on tokenization and combinations of words"
+Text3 = "Hope you Like it"
+#Combining these into one using concatenation
+Text_Overall = Text1 + " " + Text2 + " " + Text3
+#Let us choose how many words you need to combine in one line
+n = 3
+#Let us pass both the arguments to the function possible_combination
+output = possible_combination(Text_Overall,n)
+######################################################################
+#Visualizing the output
+for combibations in output:
+    # print("Please see the combinations in one line. I have set three words per line:")
+    # print("You may change the combination by settting the value of n:")
+    print(combibations)
+
